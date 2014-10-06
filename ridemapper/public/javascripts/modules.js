18a30d3185e7961c6ac2b625934043d9df042165
micahrlee <micahrlee@gmail.com>
2014-10-05 19:34:14 -0700

    address url

--- ridemapper/public/javascripts/modules.js
+++ ridemapper/public/javascripts/modules.js
@@ -2,7 +2,7 @@ var rideMapper = angular.module ('ridemapper', []);
 
 function rideMapperController($scope, $http){
 	$scope.click = function(){
-		var response = $http.get('/isValid');
+		var response = $http.get('/isValid?houseNumber=120206&street=36ave&city=Seattle&state=WA&zipCode=98125');
 		response.success(function(){
 			console.log("yay");
 		});	
