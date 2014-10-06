e70fd4e497355c4796f876efbe5558df0ccbd11e
micahrlee <micahrlee@gmail.com>
2014-10-05 19:29:16 -0700

    Connection success

--- ridemapper/public/javascripts/addressParser.js
+++
@@ -1,8 +0,0 @@
-function callIsValid(){
-	console.log(document);
-	var address = document.getElementById('address');
-	var city = document.getElementById('city');
-	var state = document.getElementById('state');
-	var zip = document.getElementById('zip');
-	$http.get("isValid");
-}
--- ridemapper/public/javascripts/modules.js
+++ ridemapper/public/javascripts/modules.js
@@ -1,1 +1,10 @@
 var rideMapper = angular.module ('ridemapper', []);
+
+function rideMapperController($scope, $http){
+	$scope.click = function(){
+		var response = $http.get('/isValid');
+		response.success(function(){
+			console.log("yay");
+		});	
+	};
+}
